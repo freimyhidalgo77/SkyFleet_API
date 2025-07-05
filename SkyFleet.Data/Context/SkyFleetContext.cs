26ac72644ea92ab227fa291c7227e38019810a95
fahh7 <fahh7@FREIMYHP>
2025-07-05 14:24:22 -0400

    Context

---
+++ SkyFleet.Data/Context/SkyFleetContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkyFleet.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyFleet.Data.Context
+{
+    public class SkyFleetContext : DbContext
+    {
+        public SkyFleetContext(DbContextOptions<SkyFleetContext> options) : base(options) { }
+
+        public DbSet<TipoVuelo> tipoVuelo { get; set; }
+
+    }
+}
