e08da885b3247eb33d6290fed0b1cdaef480aeb9
fahh7 <fahh7@FREIMYHP>
2025-07-05 14:26:11 -0400

    Abstraccion

---
+++ SkyFleet.Abstraccion/ITipoVueloService.cs
@@ -0,0 +1,21 @@
+﻿using SkyFleet.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyFleet.Abstraccion
+{
+    public interface ITipoVueloService
+    {
+        Task<bool> Guardar(TipoVueloDTO tipoVuelo);
+        Task<bool> Eliminar(int tipoVueloId);
+        Task<TipoVueloDTO> Buscar(int id);
+        Task<List<TipoVueloDTO>> Listar(Expression<Func<TipoVueloDTO, bool>> criterio);
+        Task<bool> TipoVueloExiste(int id, string descripcion);
+
+    }
+}
