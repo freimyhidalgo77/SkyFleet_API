91e39d704993c79dc94837a09ca048513834d4b7
fahh7 <fahh7@FREIMYHP>
2025-07-05 14:24:36 -0400

    DbContextRegistrar

---
+++ SkyFleet.Data/DI/DbContextRegistrar.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SkyFleet.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyFleet.Data.DI
+{
+    public static  class DbContextRegistrar
+    {
+        public static IServiceCollection RegisterDbContextFactory(this IServiceCollection services)
+        {
+            services.AddDbContextFactory<SkyFleetContext>(o => o.UseSqlite("Name=sqlConStr"));
+            return services;
+        }
+
+    }
+}
