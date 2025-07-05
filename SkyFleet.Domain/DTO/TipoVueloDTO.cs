4e69731b43263680a330c6ecde1f7d17c07272da
fahh7 <fahh7@FREIMYHP>
2025-07-05 14:26:56 -0400

    Domain

---
+++ SkyFleet.Domain/DTO/TipoVueloDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyFleet.Domain.DTO
+{
+    public class TipoVueloDTO
+    {
+        public int TipoVueloId { get; set; }
+
+        public string? nombreVuelo { get; set; }
+
+        public string? descripcionTipoVuelo { get; set; }
+
+    }
+}
