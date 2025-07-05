3847f0d94acf6dffca37866d9c7a8101a655ce6e
fahh7 <fahh7@FREIMYHP>
2025-07-05 14:25:06 -0400

    Modelo TipoVuelo

---
+++ SkyFleet.Data/Model/TipoVuelo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkyFleet.Data.Model
+{
+    public class TipoVuelo
+    {
+        [Key]
+        public int tipoVueloId { get; set; }
+
+        [Required(ErrorMessage = "Campo nombre obligatorio")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$", ErrorMessage = "El campo solo puede contener letras y espacios.")]
+        public string? nombreVuelo { get; set; }
+
+        public string? descripcionTipoVuelo { get; set; }
+
+
+    }
+}
